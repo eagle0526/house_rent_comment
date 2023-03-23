bda70e79190c5f126b17f3fb6dd245b2b93b6f24
Yee <a034506618@gmail.com>
2023-03-23 16:04:31 +0800

    devise setting

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,12 @@
+module Users
+  class RegistrationsController < Devise::RegistrationsController
+    before_action :configure_sign_up_params, only: [:create]
+  
+    private
+  
+    def configure_sign_up_params
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[nickname])
+    end
+  end
+end
+  
