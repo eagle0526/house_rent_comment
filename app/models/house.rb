49fc2ce2d446fcd76468be8bafa346bf63b8b55d
Yee <a034506618@gmail.com>
2023-04-01 21:49:01 +0800

    image normal size and show page delete image

--- app/models/house.rb
+++ app/models/house.rb
@@ -3,7 +3,8 @@ class House < ApplicationRecord
   
   belongs_to :user
   has_many_attached :images do |attachable|
-    attachable.variant :thumb, resize_to_limit: [100, 100]    
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+    attachable.variant :normal, resize_to_limit: [300, 300]
   end
 
 end
