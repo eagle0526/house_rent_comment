07bdc41993822f277f6beaf01393e1c41223f92f
Yee <a034506618@gmail.com>
2023-04-01 18:38:42 +0800

    image show and setting img variant thumb(100 x 100)

--- app/models/house.rb
+++ app/models/house.rb
@@ -2,5 +2,8 @@ class House < ApplicationRecord
   acts_as_paranoid
   
   belongs_to :user
-  has_many_attached :images
+  has_many_attached :images do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]    
+  end
+
 end
---
+++ db/migrate/20230401093259_remove_images_from_house.rb
@@ -0,0 +1,5 @@
+class RemoveImagesFromHouse < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :houses, :images
+  end
+end
