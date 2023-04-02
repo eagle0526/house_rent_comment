bd5148dd9b1735444889473b226ca3177676be00
Yee <a034506618@gmail.com>
2023-04-03 02:46:52 +0800

    card font icon and link

--- app/javascript/controllers/home_card_controller.js
+++ app/javascript/controllers/home_card_controller.js
@@ -1,10 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { faThumbsUp, faThumbsDown, faHeart, faMessageDots } from '@fortawesome/free-regular-svg-icons'
+import { faThumbsUp, faThumbsDown, faHeart, faCommentDots, faClock } from '@fortawesome/free-regular-svg-icons'
 
 export default class extends Controller {
-  initialize(){
-    library.add(faThumbsUp, faThumbsDown, faHeart, faMessageDots)
+  initialize(){    
+    library.add(faThumbsUp, faThumbsDown, faHeart, faCommentDots, faClock)
   }
   connect() {
     dom.watch()
