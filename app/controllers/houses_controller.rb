4aa63deb1aae767723d183f086be1c53ddf4ec19
Yee <a034506618@gmail.com>
2023-04-03 14:30:03 +0800

    house frendly id and babosa gem

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,7 +1,22 @@
 class HousesController < ApplicationController
+  before_action :find_house, only: [:show]
+
   def index
   end
 
   def create
   end
+
+
+  def show
+    # render html: params
+  end
+
+
+  private
+  def find_house
+    @house = House.find(params[:id])
+  end
+
+
 end
