8491291881745ef958b2faa7ba53548ef8bf3554
Yee <a034506618@gmail.com>
2023-04-11 14:35:12 +0800

    set house like_state scope

--- app/models/like_state.rb
+++ app/models/like_state.rb
@@ -1,4 +1,18 @@
 class LikeState < ApplicationRecord
   belongs_to :user
   belongs_to :likeable, polymorphic: true  
+  
+
+  # 房子點讚
+  scope :house_state_true, -> { where(state: true, likeable_type: "House") }
+  # 房子倒讚
+  scope :house_state_false, -> { where(state: false, likeable_type: "House") }
+
+  def self.house_true_count
+    self.house_state_true.size
+  end
+  def self.house_state_count
+    self.house_state_false.size
+  end
+
 end
