4b7b4acee7bac6b16271a82d1fbe012e9808beb4
Yee <a034506618@gmail.com>
2023-04-20 01:45:58 +0800

    home page click like, confirm login or not if not pop up login section

--- app/javascript/controllers/home_card_controller.js
+++ app/javascript/controllers/home_card_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { fetchWithoutParams } from "../controllers/lib/fetcher"
+import { dispatchAction } from "../controllers/lib/dispatch"
 import { faThumbsUp, faThumbsDown, faHeart, faCommentDots, faClock, faEye } from '@fortawesome/free-regular-svg-icons'
 
 
@@ -15,34 +16,49 @@ export default class extends Controller {
     dom.watch()    
   }
 
-  increase() {
-    const houseID = this.element.dataset.houseId
-    
+  increase() {          
+    const houseId = this.element.dataset.houseId
+    const userId = this.element.dataset.currentUserId
+
+    if (userId === 'Logged out') {
+      // 點擊的時候，判斷是否有登入，如果沒登入的話，觸發 nav_pop_up_controller.js 這個controller，把該section的hidden刪掉
+      // const evt = new CustomEvent("popup")
+      // window.dispatchEvent(evt)            
+      
+      // 我把他弄成一個方法，這樣之後就不用一直寫上面那兩行，只要把action傳進來就好
+      dispatchAction("popup")
+    } else {
     // 這個是新增like的路徑 - /houses/:id/like
-    fetchWithoutParams(`/houses/${houseID}/like`, "PATCH")
+    fetchWithoutParams(`/houses/${houseId}/like`, "PATCH")
       .then(({status, houseLikeCount, houseDislikeCount}) => {
-
         this.setTextContent(houseLikeCount, houseDislikeCount)
-
       })
       .catch((err) => {
         console.log(err);
-      })
+      })            
+    }  
   }
 
   decrease() {
-    const houseID = this.element.dataset.houseId
-    
-    // 這個是新增like的路徑 - /houses/:id/like
-    fetchWithoutParams(`/houses/${houseID}/dislike`, "PATCH")
-      .then(({status, houseLikeCount, houseDislikeCount}) => {
+    const houseId = this.element.dataset.houseId
+    const userId = this.element.dataset.currentUserId
 
-        this.setTextContent(houseLikeCount, houseDislikeCount)
+    if (userId === 'Logged out') {
+      // 點擊的時候，判斷是否有登入，如果沒登入的話，觸發 nav_pop_up_controller.js 這個controller，把該section的hidden刪掉
+      // const evt = new CustomEvent("popup")
+      // window.dispatchEvent(evt)     
 
-      })
-      .catch((err) => {
-        console.log(err);
-      })
+      dispatchAction("popup")
+    } else {
+      // 這個是新增like的路徑 - /houses/:id/like
+      fetchWithoutParams(`/houses/${houseId}/dislike`, "PATCH")
+        .then(({status, houseLikeCount, houseDislikeCount}) => {
+          this.setTextContent(houseLikeCount, houseDislikeCount)
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+      }
   }
 
   setTextContent(houseLikeCount, houseDislikeCount) {
