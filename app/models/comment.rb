63fa1f8827f5321d23fce6a0ab6bc576fb5db335
Yee <a034506618@gmail.com>
2023-04-20 17:57:01 +0800

    add simple_format for comment's content

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,9 +7,13 @@ class Comment < ApplicationRecord
   # 一個留言，有很多被按讚
   has_many :like_states, as: :likeable
 
-  validates :content, presence: true, length: { minimum: 5 }
+  # 至少 1 個字
+  validates :content, presence: true, length: { minimum: 1 }
 
   # 留言自聯結
   belongs_to :parent, class_name: "Comment", optional: true
   has_many :comments, foreign_key: :parent_id
+
+  # content 的 rich editor 設定
+  # has_rich_text :content  
 end
