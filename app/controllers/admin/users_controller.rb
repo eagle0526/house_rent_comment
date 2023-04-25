47c0c7b221aca2f899c9c30b8a9297efb25a322f
Yee <a034506618@gmail.com>
2023-04-25 16:10:38 +0800

    user can upload avatar and user admin wireframe

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,7 @@
 module Admin
   class UsersController < ApplicationController
     before_action :authenticate_user!
+    before_action :find_user, only: [:destroy_image]
 
     def edit          
       
@@ -10,17 +11,28 @@ module Admin
     def update
     #   render html: params
       if current_user.update(params_user)
-        redirect_to edit_admin_user_path(current_user.id), notice: "成功更新暱稱"
+        redirect_to edit_admin_user_path(current_user.id), notice: "成功更新使用者資料"
       else
-        render :edit
+        render :edit, status: :unprocessable_entity
       end
       
+    end
+
+    def destroy_image      
+
+      @user.avatar.purge
+      redirect_to edit_admin_user_path(@user.id), alert: "成功刪除圖片"      
     end
 
     private
 
     def params_user
-      params.require(:user).permit(:name)
+      params.require(:user).permit(:name, :avatar)
+    end
+
+
+    def find_user
+      @user = User.find(params[:id])
     end
   end
 end
