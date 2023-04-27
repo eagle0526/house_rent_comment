d1ebd2598c26f7f5acc1cbc931d2bfedc2300f9d
Yee <a034506618@gmail.com>
2023-04-27 13:10:48 +0800

    add user comment, like and view house filter button

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,7 +5,23 @@ module Admin
 
 
     def show
-      
+      # @houses = current_user.houses
+      message = params[:query]
+
+      if message == "已留言"
+        @houses = current_user.comments.uniq(&:house_id).map { |comment| House.find_by(id: comment.house_id) }
+      elsif message == "已點讚"
+        @houses = current_user.like_states.house_state_true.map { |like| House.find_by(id: like.likeable_id) }
+      elsif message == "已瀏覽"
+        @houses = current_user.page_views.where(impressionable_type: "House").pluck(:impressionable_id).uniq.map { |view| House.find_by(id: view) }
+      else
+        @houses = current_user.houses
+      end
+
+
+
+
+      # render html: message
     end
 
     def edit          
