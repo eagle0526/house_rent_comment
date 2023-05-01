cab3f3fb79d9a528eb1192778db39997fd2db707
Yee <a034506618@gmail.com>
2023-05-01 22:19:21 +0800

    change root path (home change house)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
   def index
-    @houses = House.all    
+    # @houses = House.all    
+    @pagy, @houses = pagy(House.all, items: 3)
+
+
+    p "*" * 50
+    p @pagy.next
+    p @houses
+    p "*" * 50
+
   end
 end
   
