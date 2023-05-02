860fbfecf7b016ed9d2655b6e2caa4ff72cdd3e2
Yee <a034506618@gmail.com>
2023-05-02 15:29:59 +0800

    revise flash css

---
+++ app/javascript/controllers/flash_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    setTimeout(() => {
+      this.element.remove();
+    }, 4000);    
+  }
+
+
+  dismiss() {
+    this.element.remove()
+  }
+}
