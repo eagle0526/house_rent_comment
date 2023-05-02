53c5ae6d94bd913639bf1b19495f559092d88ca4
Yee <a034506618@gmail.com>
2023-05-02 13:27:32 +0800

    pagy infinite scrolling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,6 @@ class HomeController < ApplicationController
   def index
     # @houses = House.all    
     @pagy, @houses = pagy(House.all, items: 3)
-
-
-    p "*" * 50
-    p @pagy.next
-    p @houses
-    p "*" * 50
-
   end
 end
   
